5a70ed06572a30eec0401ee063e760067a37a934
alexlera99 <62151164+alexlera99@users.noreply.github.com>
2020-12-09 12:10:59 +0100

    Update main.go

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func getPort() string {
 }
 
 func main() {
-   welcome := Welcome{"Alex esta contento :)", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
+   welcome := Welcome{"Argo es kk", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
 
    templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
